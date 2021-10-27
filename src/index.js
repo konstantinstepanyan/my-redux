7605b435a2e415c3180e8de884e125e56fcf5993
Konstantin <konstantinstepanyan99@gmail.com>
2021-10-28 02:17:40 +0300

    wip

--- src/index.js
+++ src/index.js
@@ -13,20 +13,29 @@ const themeBtn = document.getElementById('theme');
 
 const store = createStore(rootReducer, 0);
 
-window.store = store;
+
 
 addBtn.addEventListener('click', () => {
-    
+    store.dispatch({type: 'INCREMENT'});
 })
 
 subBtn.addEventListener('click', () => {
-   
+   store.dispatch({type: 'DECREMENT'});
 })
 
 asyncBtn.addEventListener('click', () => {
   
 });
 
+
+store.subscribe(() => {
+    const state = store.getState();
+    
+    counter.textContent = state;
+});
+
+
+store.dispatch({type: 'INIT_APPLICATION'})
 
 
 themeBtn.addEventListener('click', () => {
